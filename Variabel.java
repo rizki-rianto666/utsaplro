bb8655ebdfce33fc980230922fce9183c3ba2e67
rizki-rianto666 <115854620+rizki-rianto666@users.noreply.github.com>
2022-11-19 09:53:32 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,9 @@
+public class Variabel {
+	public static void main(String[]args) {
+		String nama = "M. Rizki Rianto";
+		int tahun	= 2004;
+		System.out.println("Assalamu'alaikum");
+		System.out.println("Nama saya : " + nama);
+		System.out.println("Saya lahir di tahun : " + tahun);
+	}
+}
